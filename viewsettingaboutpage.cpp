46acf58277c8fcb3a48540d30fc6294cb7e663ea
sharkpp <touroku@sharkpp.net>
2017-05-01 01:21:20 +0900

    バージョン情報にアプリケーションアイコンを表示

--- viewsettingaboutpage.cpp
+++ viewsettingaboutpage.cpp
@@ -1,12 +1,18 @@
 #include "viewsettingaboutpage.h"
 #include "ui_viewsettingaboutpage.h"
+#include <QFileInfo>
+#include <QDir>
+#include <QDebug>
 
 ViewSettingAboutPage::ViewSettingAboutPage(QWidget *parent) :
     QFrame(parent),
     ui(new Ui::ViewSettingAboutPage)
 {
     ui->setupUi(this);
-    ui->appIcon->setPixmap(QIcon(":/icons.white/add.svg").pixmap(QSize(48, 48)));
+#ifdef Q_OS_MAC
+    QFileInfo iconPath = QFileInfo(QDir(qApp->applicationDirPath()), "../Resources/icon-macOS.icns");
+    ui->appIcon->setPixmap(QIcon(iconPath.absoluteFilePath()).pixmap(ui->appIcon->geometry().size()));
+#endif
 }
 
 ViewSettingAboutPage::~ViewSettingAboutPage()
